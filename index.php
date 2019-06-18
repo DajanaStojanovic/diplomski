c4339aa6c386a68913186f94607de865d478af30
Dajana <daja15@live.com>
2019-06-19 01:32:00 +0200

    home done
    home done

--- index.php
+++ index.php
@@ -8,7 +8,38 @@
 	<a href="brojIgraca.php" class="buttonKreiraj">KREIRAJ</a>
 </div>
 
-
+<div class="homeContainer">
+	<div class="brojIgracaText homeText">
+		Biraj za koliko igrača želiš napraviti igru
+	</div>
+	<div class="brojIgracaImage homeImage">
+		<img alt="logo" src="images/homeIgraci.png" class="Igrači" />
+	</div>
+	<div class="bojaText homeText">
+		Biraj boju pozadine svoje ploče
+	</div>
+	<div class="bojaImage homeImage">
+		<img alt="logo" src="images/homeBoja.png" class="Odabir boje" />
+	</div>
+	<div class="figuriceText homeText">
+		Biraj kakvu vrstu figurica želiš
+	</div>
+	<div class="figuriceImage homeImage">
+		<img alt="logo" src="images/homeFigurice.png" class="Odabir figurica" />
+	</div>
+	<div class="videoText homeText">
+		Dodaj postojeća pravila ili kreiraj nova
+	</div>
+	<div class="videoImage homeImage">
+		<img alt="logo" src="images/homeVideo.png" class="Video" />
+	</div>
+	<div class="postarText homeText">
+		Popuni svoje podatke i igra ti stiže na kućnu adresu
+	</div>
+	<div class="postarImage homeImage">
+		<img alt="logo" src="images/homePostar.png" class="Video" />
+	</div>
+</div>
 
 
 <?php include 'footer.php'?>
