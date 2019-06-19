fc8e02ad9ac2fa93604404955a181ee9bfbceff8
Dajana <daja15@live.com>
2019-06-19 22:16:04 +0200

    Odabir oblika ploče
    Radi

--- odabirOblikaPloce.php
+++ odabirOblikaPloce.php
@@ -5,10 +5,20 @@ if ($veza === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
+
 $izraz = $veza->prepare("select * from oblik_ploce where broj_igraca=:broj_igraca");
 $izraz -> bindParam(":broj_igraca", $_GET["brojIgraca"]);
 $izraz->execute();
 $rezultati = $izraz->fetchAll(PDO::FETCH_OBJ);
+
+if(isset($_POST["buttonDalje"])){
+	$updateNaraudzba=$veza->prepare("update narudzba set
+		oblik_ploce=:oblik_ploce
+		where id=:id");
+        $updateNaraudzba->bindParam(":oblik_ploce", $_POST['oblikPloce']);
+        $updateNaraudzba->bindParam(":id", $_POST['brojIgracaId']);
+        $updateNaraudzba->execute();
+};
 ?>
 
 <div class="headerPages">
@@ -27,6 +37,7 @@ $rezultati = $izraz->fetchAll(PDO::FETCH_OBJ);
 	            <input type="radio" name="oblikPloce" class="radioButton" value="<?php echo $r->id ?>">
 	        </div>
 	    <?php endforeach; ?>
+	    
 	</div>
 
 
@@ -37,18 +48,8 @@ $rezultati = $izraz->fetchAll(PDO::FETCH_OBJ);
 
 		<input class="buttonDalje" type="submit" name="buttonDalje" value="DALJE" disabled/>
 	</div>
+	<input type="hidden" name="brojIgracaId" value="<?php echo $_GET['id'] ?>">
 </form>
 
 
-<?php if(isset($_POST["buttonDalje"])){
-	$updateNaraudzba=$veza->prepare("update narudzba set
-		oblik_ploce=:oblik_ploce
-		where id=:id");
-        $updateNaraudzba->bindParam(":oblik_ploce", $_POST['oblikPloce']);
-        $updateNaraudzba->bindParam(":id", $_GET['id']);
-        $updateNaraudzba->execute();
- echo  $_POST['oblikPloce'];
-
-};
-
-include 'footer.php' ?>
+<?php include 'footer.php' ?>
